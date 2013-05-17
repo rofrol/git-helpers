d719f121b505e1533d0753961611f986b7f34521
Roman Frołow <r.frolow@samsung.com>
2013-05-17 12:44:34 +0200

    fix typo with set-git-val.bash

--- set-git-val.bash
+++ set-git-val.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 #http://stackoverflow.com/questions/9660608/how-can-i-check-if-a-directory-is-on-the-path-in-bash
+
+#wget -q -O - "https://raw.github.com/rofrol/git-helpers/master/set-get-val.bash" | bash
+#or
+#curl "https://raw.github.com/rofrol/git-helpers/master/set-get-val.bash" | bash
 home_bin=$HOME/bin
 
 if [ ! -d "$home_bin" ]; then
@@ -12,4 +16,4 @@ if [[ :$PATH: != *:"$home_bin":* ]] ; then
     echo "PATH=$PATH:$HOME/bin" > ~/.bashrc
 fi
 
-cd $home_bin && wget https://raw.github.com/rofrol/git-helpers/master/get-val && chmod +x $home_bin/git-val
+cd $home_bin && wget https://raw.github.com/rofrol/git-helpers/master/git-val && chmod +x $home_bin/git-val
