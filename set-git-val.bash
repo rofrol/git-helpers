bd970a67f2ce69a7d23614bf78212b35c6e53447
Roman Frołow <r.frolow@samsung.com>
2013-05-17 12:51:18 +0200

    ups, that could be dangerous

--- set-git-val.bash
+++ set-git-val.bash
@@ -13,7 +13,7 @@ fi
 
 if [[ :$PATH: != *:"$home_bin":* ]] ; then
     echo "adding \$HOME/bin to \$PATH"
-    echo "PATH=$PATH:$HOME/bin" > ~/.bashrc
+    echo "PATH=\$PATH:\$HOME/bin" >> ~/.bashrc
 fi
 
 cd $home_bin && wget https://raw.github.com/rofrol/git-helpers/master/git-val && chmod +x $home_bin/git-val
